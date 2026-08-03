8a59c72e21a551100e5e4cd3c93dedbcff8df767
Pedro Gardim <me@pedrogardim.com>
2026-08-03 17:48:27 -0300

    feat: enhance VolunteersService tests with exception handling scenarios
    Add unit tests to VolunteersService for handling conflict and not found exceptions during volunteer creation, updating, and removal. Implement checks for ConflictException when a volunteer already exists and NotFoundException when attempting to update or remove a non-existent volunteer.

--- src/volunteers/volunteers.service.spec.ts
+++ src/volunteers/volunteers.service.spec.ts
@@ -3,6 +3,8 @@ import { VolunteersService } from './volunteers.service';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { VolunteersRepository } from './volunteers.repository';
 import { CreateVolunteerDto } from './dto/create-volunteer.dto';
+import { UpdateVolunteerDto } from './dto/update-volunteer.dto';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 
 describe('VolunteersService', () => {
   let service: VolunteersService;
@@ -32,10 +34,52 @@ describe('VolunteersService', () => {
     it('should connect given user id to created volunteer', async () => {
       const id = 'user-id';
       const createDto = createMock<CreateVolunteerDto>();
+
+      volunteersRepository.findByUserId.mockResolvedValue(null);
+
       await service.create(id, createDto);
 
       expect(volunteersRepository.create).toHaveBeenCalledWith(
         expect.objectContaining({ user: { connect: { id } } }),
+      );
+    });
+
+    it('should throw ConflictException if volunteer already exists', async () => {
+      const id = 'user-id';
+      const createDto = createMock<CreateVolunteerDto>();
+      volunteersRepository.findByUserId.mockResolvedValue(
+        createMock() as Awaited<
+          ReturnType<VolunteersRepository['findByUserId']>
+        >,
+      );
+
+      await expect(service.create(id, createDto)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+  });
+
+  describe('updateByUserId', () => {
+    it('should throw NotFoundException if volunteer does not exist', async () => {
+      const id = 'user-id';
+      const updateDto = createMock<UpdateVolunteerDto>();
+
+      volunteersRepository.findByUserId.mockResolvedValue(null);
+
+      await expect(service.updateByUserId(id, updateDto)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('removeByUserId', () => {
+    it('should throw NotFoundException if volunteer does not exist', async () => {
+      const id = 'user-id';
+
+      volunteersRepository.findByUserId.mockResolvedValue(null);
+
+      await expect(service.removeByUserId(id)).rejects.toThrow(
+        NotFoundException,
       );
     });
   });
